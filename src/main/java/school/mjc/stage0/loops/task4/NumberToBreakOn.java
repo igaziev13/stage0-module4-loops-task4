f1f611ccad11b5cc655e2045fc2232360aa656a9
IGAZIEV <islam.gaziev@gmail.com>
2022-08-08 14:07:01 +0500

    Module #4.2. Task #4. Attempt #2.

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -4,9 +4,9 @@ public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (numberToGoUntil<toBreakWith)
             System.out.println("iterating till the end");
-        for (int i=1;i<=numberToGoUntil;i++)
+        for (int i = 1; i <= numberToGoUntil; i++)
         {
-            if (toBreakWith==i)
+            if (toBreakWith+1==i)
                 break;
             System.out.println(i);
         }
